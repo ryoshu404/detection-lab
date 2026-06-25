2424363837de69f3711c447fa8ebfaaa86ff6e7e
ryoshu404 <192475624+ryoshu404@users.noreply.github.com>
2026-06-25 10:21:35 -0500

    feat: retire EC2 SIEM + NAT, migrate to local Elastic

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "siem_instance_id" {
-  description = "SIEM EC2 instance ID for SSM session targeting"
-  value       = module.siem.siem_instance_id
-}
-
 output "cloudtrail_sqs_queue_url" {
   description = "CloudTrail notification queue URL"
   value       = module.detection.cloudtrail_sqs_queue_url
